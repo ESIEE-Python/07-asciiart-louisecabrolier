87ad61d3f5fd1685f43818fa52c8366a1f185a74
Louise Cabrolier <louise.cabrolier@edu.esiee.fr>
2024-09-24 08:50:16 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
-#### Imports et définition des variables globales
-
-
-#### Fonctions secondaires
-
-
+"""
+ASCII
+"""
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
-
-    Args:
-        s (str): la chaîne de caractères à encoder
-
-    Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
-
+    entrée:s la chaine de caractère à encoder
+    sortie:liste de tuples encodant une chaîne de caractères passée en argument
+    itératif
+    """
+    if not s:
+        return []
+    # Initialisation
+    c = [s[0]]
+    o = [1]
+    n = len(s)
+    for k in range(1, n):
+        if s[k] == s[k - 1]:
+            o[-1] += 1
+        else:
+            c.append(s[k])
+            o.append(1)
+    return list(zip(c, o))
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
-
-    Args:
-        s (str): la chaîne de caractères à encoder
-
-    Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
-
-#### Fonction principale
-
+    entrée: s
+    sortie:liste de tuples encodant une chaîne de caractères
+    récursif
+    """
+    if not s:
+        return []
+    def count_consecutive(s: str, char: str, index: int = 0):
+        if index < len(s) and s[index] == char:
+            return 1 + count_consecutive(s, char, index + 1)
+        return 0
+    count = count_consecutive(s, s[0])
+    return [(s[0], count)] + artcode_r(s[count:])
 
 def main():
+    """
+    entrée:une chaine de caractère
+    sortie:le programme appliqué
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
